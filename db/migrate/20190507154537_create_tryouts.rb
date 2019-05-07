c7a64b783e8e9d47658bcf95580da99047af6e67
Han Xu <hanxu27@gmail.com>
2019-05-07 11:57:30 -0400

    created tryouts table

---
+++ db/migrate/20190507154537_create_tryouts.rb
@@ -0,0 +1,19 @@
+class CreateTryouts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tryouts do |t|
+      t.integer :player_id
+      t.integer :coach_id
+      t.string :string
+      t.integer :age_level
+      t.integer :tryout_number
+      t.integer :player_height
+      t.string :player_position
+      t.integer :passing
+      t.integer :setting
+      t.integer :hitting
+      t.integer :emotions
+      t.integer :talking
+      t.integer :learning
+    end
+  end
+end
