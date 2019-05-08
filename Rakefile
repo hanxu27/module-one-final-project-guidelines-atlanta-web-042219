bbe6cb44cdc07f1633b4d66ee09840aee16d5da3
Han Xu <hanxu27@gmail.com>
2019-05-08 11:14:36 -0400

    added player methods: view_eval, view_coaches. Added coach methods: clear_players, new_eval(unfinished).

--- Rakefile
+++ Rakefile
@@ -4,6 +4,14 @@ require 'sinatra/activerecord/rake'
 desc 'starts a console'
 task :console do
   # ActiveRecord::Base.logger.level = Logger.new(STDOUT)
-  ActiveRecord::Base.logger.level = nil
-  Pry.start
+  # ActiveRecord::Base.logger.level = nil
+
+  p = Player.first
+  p.sign_up(age_level: 14, position: "MB")
+  c = Coach.first
+  
+  
+  binding.pry
+
+  c.new_eval(tryout_number: p.tryout_number, setting: 8)
 end
--- lib/coach.rb
+++ lib/coach.rb
@@ -1,4 +1,21 @@
 class Coach < ActiveRecord::Base
     has_many :tryouts
     has_many :players, through: :tryouts
+
+    def new_eval(tryout_number:, setting:)
+        # , passing:, hitting:, emotions:, talking:, learning:
+        player = Player.find(tryout_number)
+        tryout = Tryout.create(player_id: player.id, coach_id: self.id, setting: setting)
+        # , passing: passing, hitting: hitting, emotions: emotions, talking: talking, learning: learning
+        tryout.age_level = player.age_level
+        tryout.player_position = player.position
+        tryout.tryout_number = tryout_number
+        tryout.save
+    end
+
+    def clear_players
+        #clear players who have not signed up
+        Player.where(tryout_number: nil).destroy_all
+    end
+
 end
